e30f308591dfcfcedd3b75bf75f9bed0c89fa024
Carlos Carrera <carlos.carrera.sge@mango.com>
2022-10-27 18:08:20 +0100

    Initial Commit

---
+++ RickAndMorty/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  RickAndMorty
+//
+//  Created by Carlos Carrera on 27/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ RickAndMorty/RickAndMortyApp.swift
@@ -0,0 +1,17 @@
+//
+//  RickAndMortyApp.swift
+//  RickAndMorty
+//
+//  Created by Carlos Carrera on 27/10/22.
+//
+
+import SwiftUI
+
+@main
+struct RickAndMortyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
